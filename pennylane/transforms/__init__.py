9275248a00d9707ebd72c1894383b7c5886ea683
Cenk Tüysüz <cenktuysuz@gmail.com>
2022-10-06 16:01:44 +0200

    added missing changes

--- pennylane/transforms/__init__.py
+++ pennylane/transforms/__init__.py
@@ -180,6 +180,7 @@ Transforms for error mitigation
     ~transforms.mitigate_with_zne
     ~transforms.fold_global
     ~transforms.poly_extrapolate
+    ~transforms.exp_extrapolate
     ~transforms.richardson_extrapolate
 """
 # Import the decorators first to prevent circular imports when used in other transforms
@@ -200,7 +201,7 @@ from .measurement_grouping import measurement_grouping
 from .metric_tensor import metric_tensor
 from .adjoint_metric_tensor import adjoint_metric_tensor
 from .insert_ops import insert
-from .mitigate import mitigate_with_zne, fold_global, poly_extrapolate, richardson_extrapolate
+from .mitigate import mitigate_with_zne, fold_global, poly_extrapolate, exp_extrapolate, richardson_extrapolate
 from .optimization import (
     cancel_inverses,
     commute_controlled,
